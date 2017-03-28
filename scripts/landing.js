5ed22aba6711cc69946a251dddab875e0cf35bb1
Steve Lanier <sklanier@outlook.com>
2017-03-27 18:51:30 -0600

    assignment 9 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,12 +1,12 @@
-var animatePoints = function() {
+var animatePoints = function () {
  
                  var points = document.getElementsByClassName('point');
  
-                 var revealFirstPoint = function() {
-                     points[0].style.opacity = 1;
-                     points[0].style.transform = "scaleX(1) translateY(0)";
-                     points[0].style.msTransform = "scaleX(1) translateY(0)";
-                     points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+                 var revealPoint = function(index) {
+                     points[index].style.opacity = 1;
+                     points[index].style.transform = "scaleX(1) translateY(0)";
+                     points[index].style.msTransform = "scaleX(1) translateY(0)";
+                     points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
                  };
  
                  var revealSecondPoint = function() {
@@ -22,9 +22,7 @@ var animatePoints = function() {
                      points[2].style.msTransform = "scaleX(1) translateY(0)";
                      points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
                  };
- 
-                 revealFirstPoint();
-                 revealSecondPoint();
-                 revealThirdPoint();
- 
+								for (var i=0; i<points.length; i++){
+									revealPoint(i);
+								}
              };
