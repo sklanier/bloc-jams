752415181e02f15a935bc8ba25b8ebee85dddf47
Steve Lanier <sklanier@outlook.com>
2017-03-28 11:00:47 -0600

    checkpoint 9 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function() {
+ var pointsArray = document.getElementsByClassName('point');
  
-                 var points = document.getElementsByClassName('point');
+ var animatePoints = function(points) {
  
                  var revealFirstPoint = function() {
                      points[0].style.opacity = 1;
@@ -28,3 +28,19 @@ var animatePoints = function() {
                  revealThirdPoint();
  
              };
+
+ window.onload = function() {
+			if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+		  }
+			var sellingPoints = document.getElementsByClassName('selling-points')[0];
+	    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+
+			window.addEventListener('scroll', function(event) {
+					console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
+				  if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }
+     });
+ }
