2214eb840cb0505dd2a63bba0090ab320d9e22af
Steve Lanier <sklanier@outlook.com>
2017-03-31 08:44:32 -0600

    assignment 9 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,33 +1,15 @@
  var pointsArray = document.getElementsByClassName('point');
- 
+
+var revealPoint = function(point) {
+			 point.style.opacity = 1;
+			 point.style.transform = "scaleX(1) translateY(0)";
+			 point.style.msTransform = "scaleX(1) translateY(0)";
+			 point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
+	
  var animatePoints = function(points) {
- 
-                 var revealFirstPoint = function() {
-                     points[0].style.opacity = 1;
-                     points[0].style.transform = "scaleX(1) translateY(0)";
-                     points[0].style.msTransform = "scaleX(1) translateY(0)";
-                     points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
- 
-                 var revealSecondPoint = function() {
-                     points[1].style.opacity = 1;
-                     points[1].style.transform = "scaleX(1) translateY(0)";
-                     points[1].style.msTransform = "scaleX(1) translateY(0)";
-                     points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
- 
-                 var revealThirdPoint = function() {
-                     points[2].style.opacity = 1;
-                     points[2].style.transform = "scaleX(1) translateY(0)";
-                     points[2].style.msTransform = "scaleX(1) translateY(0)";
-                     points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
- 
-                 revealFirstPoint();
-                 revealSecondPoint();
-                 revealThirdPoint();
- 
-             };
+ 				forEach(points, revealPoint);
+};
 
  window.onload = function() {
 			if (window.innerHeight > 950) {
