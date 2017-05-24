275a7eee09dac9eea9de14b71a838800354cc49b
Steve Lanier <sklanier@outlook.com>
2017-05-23 20:15:59 -0600

    checkpoint 17 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,46 +1,23 @@
- var pointsArray = document.getElementsByClassName('point');
- 
- var animatePoints = function(points) {
- 
-                 var revealFirstPoint = function() {
-                     points[0].style.opacity = 1;
-                     points[0].style.transform = "scaleX(1) translateY(0)";
-                     points[0].style.msTransform = "scaleX(1) translateY(0)";
-                     points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
- 
-                 var revealSecondPoint = function() {
-                     points[1].style.opacity = 1;
-                     points[1].style.transform = "scaleX(1) translateY(0)";
-                     points[1].style.msTransform = "scaleX(1) translateY(0)";
-                     points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
- 
-                 var revealThirdPoint = function() {
-                     points[2].style.opacity = 1;
-                     points[2].style.transform = "scaleX(1) translateY(0)";
-                     points[2].style.msTransform = "scaleX(1) translateY(0)";
-                     points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
- 
-                 revealFirstPoint();
-                 revealSecondPoint();
-                 revealThirdPoint();
- 
-             };
+var animatePoints = function() {
+		var revealPoint = function() {
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
+		};
+	
+		$.each($('.point'), revealPoint);
+};
 
- window.onload = function() {
-			if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
-		  }
-			var sellingPoints = document.getElementsByClassName('selling-points')[0];
-	    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+	$(window).load(function() {
+			if ($(window).height() > 950) {
+       animatePoints();
+			}
+			var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
-
-			window.addEventListener('scroll', function(event) {
-					console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
-				  if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);   
-         }
-     });
- }
+	  $(window).scroll(function(event) {
+	  	if ($(window).scrollTop() >= scrollDistance) {
+	  	animatePoints();  
+	  	} 
+    });
+ });
